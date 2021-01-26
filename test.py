c423d7ef97f7ac3e7302de766566af087a4d1459
ren-ishibashi <yoshinobu.24ren0910@gmail.com>
2021-01-26 16:02:42 +0900

    add a file pythonenter

---
+++ test.py
@@ -0,0 +1,34 @@
+#comment
+# print("Hello World")
+
+# 演算
+# print(1+1)
+# print(1-1)
+# print(2*2)
+# print(10/2)
+# print(5%3)
+
+# 変数
+# unko = 'l_size'
+# login_id = 'sihisincfb0293'
+# unko_length = 0
+# unko_times = 5.5
+
+# print(type(unko))
+# print(unko_times)
+# print(unko_times)
+
+# unko_shitai = False #ブーリアン型
+
+# 条件分岐と関係演算子
+# if else elif
+# if unko_length > 7:
+#     print('ooi')
+# elif unko_length == 0:
+#     print('nai')
+# else:
+#     print('sukunai')
+    
+# 関数
+    
+    
